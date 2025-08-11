73e12438107bc060b71fe116822a639032881314
Mario Hernandez <marioisrahf@gmail.com>
2025-08-11 02:24:08 -0400

    Specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
     //... Se pueden agregar más registros de empleados aquí
   ];
 
@@ -29,3 +29,9 @@ function findEmployeeById(employeeId) {
       document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con este ID';
      }
  }
+
+ function findBySpecialization() {
+    const spEmployees = employees.filter(employee => employee.specialization === 'Javascript');
+    const spEmployeesDisplay = spEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - $${employee.specialization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = spEmployeesDisplay;
+}

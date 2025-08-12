82cc17b76733d8ef79e2383db94679b290d7e229
Mario Hernandez <marioisrahf@gmail.com>
2025-08-12 18:37:04 -0400

    New Operations

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,18 +1,49 @@
     
-    function performOperation() {
+    function performOperation(operationType) {
         // Get user input from input fields
         let num1 = parseInt(document.getElementById('input1').value);
         let num2 = parseInt(document.getElementById('input2').value);
         // Check if inputs are valid numbers
         if (!isNaN(num1) && !isNaN(num2)) {
-        // Perform the operation
-                        let result = multiply(num1, num2);
+            let result;
     
-                        // Display the result
-                        displayResult(result);
-                    } else {
-                        displayResult('Please enter valid numbers');
-                    }
+            // Usar una declaración `switch` para determinar la operación
+            switch (operationType) {
+                case 'sum':
+                    result = sum(num1, num2);
+                    break;
+                case 'rest':
+                    result = rest(num1, num2);
+                    break;
+                case 'multiply':
+                    result = multiply(num1, num2);
+                    break;
+                case 'div':
+                    result = div(num1, num2);
+                    break;
+                default:
+                    result = 'Operación no válida';
+            }
+    
+            // Mostrar el resultado
+            displayResult(result);
+        } else {
+            displayResult('Por favor, ingresa números válidos');
+        }
+                }
+                function sum(a,b){
+                    debugger;
+                    return a+b;
+                }
+
+                function rest(a,b){
+                    debugger;
+                    return a-b;
+                }
+
+                function div(a,b){
+                    debugger;
+                    return a/b;
                 }
     
                 function multiply(a, b) {

93a0321d2c810f34d9d0345d3f9252ff8ceab453
Mario Hernandez <marioisrahf@gmail.com>
2025-08-13 01:52:29 -0400

    Delete button

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
@@ -36,7 +37,13 @@ function displayTasks() {
             displayTasks();
         }
 
+        function clearAllTasks() {
+            tasks = [];
+            displayTasks();
+        }
+
         addTaskBtn.addEventListener("click", addTask);
         clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+        clearAllBtn.addEventListener("click", clearAllTasks);
 
         displayTasks();
